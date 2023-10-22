e562dee7235587c7f7c2fd17789cca153d6715a8
wreifschneider <124732320+wreifschneider@users.noreply.github.com>
2023-10-22 14:39:08 +0000

    Copilot third commit

--- member.js
+++ member.js
@@ -1,16 +1,43 @@
-// Create web ser
+// Create web server
+import express from 'express';
+import bodyParser from 'body-parser';
+import memberRouter from './routes/member.js';
 
-import { createServer } from 'http';
+// Create express app
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// Parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Configuring the database
+import dbConfig from './config/database.js';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Successfully connected to the database');
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Define a simple route
+app.get('/', (req, res) => {
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+});
+
+// Use member routes
+app.use('/members', memberRouter);
+
+// Listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
